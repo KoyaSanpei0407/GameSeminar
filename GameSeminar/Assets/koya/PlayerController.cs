f11a08140208826d834f0c191b504f24e6791de7
negudevil <negudevil47@gmail.com>
2020-02-14 16:59:28 +0900

    プレイヤーの移動

---
+++ GameSeminar/Assets/koya/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;       //歩行速度
+    private float lh, lv, rh, rv;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Lスティックの入力
+        lh = Input.GetAxis("Controller1_LStickX");
+        lv = Input.GetAxis("Controller1_LStickY");
+
+        if ((lh != 0) || (lv != 0))
+        {
+            Vector3 pos = transform.position;
+            pos.y += speed * Time.deltaTime * lv;
+            pos.x += speed * Time.deltaTime * lh;
+            transform.position = pos;
+        }
+
+        rh = Input.GetAxis("Controller1_LStickX");
+        rv = Input.GetAxis("Controller1_LStickY");
+        
+
+        if ((rh != 0) || (rv != 0))
+        {
+
+            var angle = (Mathf.Atan2(rv, rh) * Mathf.Rad2Deg) - 90.0f;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+        }
+
+
+    }
+}
---
+++ GameSeminar/Assets/koya/TimerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text timerText;
+    public GameObject text1;
+
+    public float totalTime;
+    int seconds;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        seconds = (int)totalTime;
+        timerText.text = seconds.ToString();
+
+        if(totalTime <= 0)
+        {
+            //文字を消す
+            text1.SetActive(false);
+        }
+    }
+}
