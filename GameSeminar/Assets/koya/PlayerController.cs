33381258807babe47f880383bde741cb81158d90
negudevil <negudevil47@gmail.com>
2020-02-15 10:02:33 +0900

    プレイヤー２の追加

--- GameSeminar/Assets/koya/PlayerController.cs
+++ GameSeminar/Assets/koya/PlayerController.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed;       //歩行速度
+    private int hp;
+    public float speed;
+    public string pnum;
     private float lh, lv, rh, rv;
-    
+
     void Start()
     {
+        hp = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Lスティックの入力
-        lh = Input.GetAxis("Controller1_LStickX");
-        lv = Input.GetAxis("Controller1_LStickY");
+        lh = Input.GetAxis("Controller" + pnum + "_LStickX");
+        lv = Input.GetAxis("Controller" + pnum  + "_LStickY");
 
         if ((lh != 0) || (lv != 0))
         {
@@ -27,17 +29,23 @@ public class PlayerController : MonoBehaviour
             transform.position = pos;
         }
 
-        rh = Input.GetAxis("Controller1_LStickX");
-        rv = Input.GetAxis("Controller1_LStickY");
-        
+        //Rスティックの入力
+        rh = Input.GetAxis("Controller" + pnum + "_RStickX");
+        rv = Input.GetAxis("Controller" + pnum + "_RStickY");
+
 
         if ((rh != 0) || (rv != 0))
         {
-
             var angle = (Mathf.Atan2(rv, rh) * Mathf.Rad2Deg) - 90.0f;
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
         }
+    }
 
-
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "bullet")
+        {
+            hp -= 1;
+        }
     }
 }
