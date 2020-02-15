f0c8b962a8478b3d865e1c15877ae50d9fe0e39f
吉田崇信 <zeal18300136@c-league.jp>
2020-02-15 22:02:06 +0900

    統合したやつ

---
+++ GameSeminar/Assets/B_bulletGenerate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_bulletGenerate : MonoBehaviour
+{
+    public int bulletnumber;//弾の数
+    float x;
+    float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = Random.Range(-8, 8);
+        y = Random.Range(-5, 5);
+        for (int i = 0; i <= bulletnumber; i++)
+        {
+            GameObject obj = (GameObject)Resources.Load("Bombbullet");
+            GameObject instantiate = Instantiate(obj, new Vector2(x, y), Quaternion.identity);//弾の生成される位置
+        }
+    }
+        // Update is called once per frame
+        void Update()
+    {
+    }
+}
---
+++ GameSeminar/Assets/Sato/script/bomb.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    public int bulletnumber=25;//弾の数
+    float x;
+    float y;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "P2_bullet")
+        {
+            //BulletだったらBombが消える
+            Destroy(this.gameObject);
+        }
+        if (col.gameObject.tag == "P1_bullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        x = Random.Range(-8, 8);
+        y = Random.Range(-5, 5);
+        for (int i = 0; i <= bulletnumber; i++)
+        {
+            GameObject obj = (GameObject)Resources.Load("BomBullet");
+            GameObject instantiate = Instantiate(obj, new Vector2(x, y), Quaternion.identity);//弾の生成される位置
+        }
+    }
+
+}
