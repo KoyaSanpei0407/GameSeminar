86412060854bb22bc05ef0879506c6fa788c0df2
ryo2020214 <zeal19300127@c-league.jp>
2020-02-15 12:12:58 +0900

    テストコミット

---
+++ GameSeminar/Assets/Master/Script/HpBarCtl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarCtl : MonoBehaviour
+{
+    public GameObject player;
+    Slider slider;
+    public float Y_pos = -0.5f;
+    float m_Hp = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y = pos.y + Y_pos;
+        transform.position = pos;
+
+        slider.value = m_Hp;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag== "bullet")
+        {
+            m_Hp -= 1;
+        }
+    }
+}
---
+++ GameSeminar/Assets/koya/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int hp;
+    public float speed;
+    public string pnum;
+    private float lh, lv, rh, rv;
+
+    void Start()
+    {
+        hp = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Lスティックの入力
+        lh = Input.GetAxis("Controller" + pnum + "_LStickX");
+        lv = Input.GetAxis("Controller" + pnum  + "_LStickY");
+
+        if ((lh != 0) || (lv != 0))
+        {
+            Vector3 pos = transform.position;
+            pos.y += speed * Time.deltaTime * lv;
+            pos.x += speed * Time.deltaTime * lh;
+            transform.position = pos;
+        }
+
+        //Rスティックの入力
+        rh = Input.GetAxis("Controller" + pnum + "_RStickX");
+        rv = Input.GetAxis("Controller" + pnum + "_RStickY");
+
+
+        if ((rh != 0) || (rv != 0))
+        {
+            var angle = (Mathf.Atan2(rv, rh) * Mathf.Rad2Deg) - 90.0f;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "bullet")
+        {
+            hp -= 1;
+        }
+    }
+}
