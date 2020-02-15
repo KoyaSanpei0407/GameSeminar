fe0c1663074ca7a61ed4e388064c4eac7bde3dae
ryo2020214 <zeal19300127@c-league.jp>
2020-02-15 12:11:27 +0900

    爆弾の弾を実装

---
+++ GameSeminar/Assets/B_bulletGenerate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_bulletGenerate : MonoBehaviour
+{
+    public int bulletnumber;//弾の数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i <= bulletnumber; i++)
+        {
+            GameObject obj = (GameObject)Resources.Load("Bombbullet");
+            GameObject instantiate = Instantiate(obj, new Vector2(0, 0), Quaternion.identity);//弾の生成される位置
+        }
+    }
+        // Update is called once per frame
+        void Update()
+    {
+    }
+}
